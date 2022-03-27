bb3f7c86ad993faac178326eb5433fcdbf0ad319
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-03-27 19:24:05 +0100

    High Scores Worldwide Complete

--- src/HighScores.js
+++ src/HighScores.js
@@ -2,33 +2,44 @@ import React from "react";
 
 function HighScores(props) {
   let newArray = [];
+  let scoreArray = [];
+  let nameArray = [];
+  const obj = {};
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 5; j++) {
       if (props.scores[i].scores[j]) {
         let score = props.scores[i].scores[j].s;
+        let nameOfPlayer = props.scores[i].scores[j].n;
         newArray.push(score);
+        scoreArray.push(score);
+        nameArray.push(nameOfPlayer);
       }
     }
   }
 
+  nameArray.forEach((element, index) => {
+    obj[element] = scoreArray[index];
+  });
+
   let topFive = newArray.sort((a, b) => b - a).splice(0, 5);
 
   return (
-    <div>
+    <div className="cards">
       {/* {console.log(props.scores[0].scores[0].s)} */}
-      {console.log(topFive)}
-      High Scores Worldwide
-      <table>
+      {console.log(obj)}
+      HIGH SCORES: Worldwide
+      <table className="styled-table">
         <tbody>
           {topFive.map((el) => {
             return (
               <tr>
+                {Object.keys(obj)
+                  .find((key) => obj[key] === el)
+                  .toUpperCase()}
                 <td>{el}</td>
               </tr>
             );
           })}
-
-          <tr></tr>
         </tbody>
       </table>
     </div>
---
+++ src/ToggleScore.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ToggleScore() {
+  return <div>High Scores per Country</div>;
+}
+
+export default ToggleScore;
